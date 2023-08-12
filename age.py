695a7af4cab7976efef28acc9dae553f699dc2ba
K.ShivaNagendra <40945928+shivanagendrak@users.noreply.github.com>
2023-08-12 11:50:02 -0400

    Delete age.py

--- age.py
+++
@@ -1,9 +0,0 @@
-first = int(input("Enter the First Age: "))
-second = int(input("Enter the Second Age: "))
-third = int(input("Enter the Third Age: "))
-if first>second:
-    print("Oldest Age is : ",first)
-elif second>third:
-    print("Oldest Age is : ",second)
-elif third > first:
-    print("Oldest Age is : ",third)
